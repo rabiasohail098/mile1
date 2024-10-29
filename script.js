eb1f87cee00954b743a3c905213c7651b57fd3af
rabiasohail098 <rabiasohail1209@gmail.com>
2024-10-29 08:01:48 +0500

    updated

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var toggleButton = document.getElementById("toggle-skills");
 var skills = document.getElementById("skills");
 toggleButton.addEventListener("click", function () {
-    if (skills.style.display == "none" || !skills.style.display) {
-        skills.style.display = "block";
+    if (skills.style.display == "block" || !skills.style.display) {
+        skills.style.display = "none";
     }
     else {
-        skills.style.display = "none";
+        skills.style.display = "block";
     }
 });
--- script.ts
+++ script.ts
@@ -1,11 +1,11 @@
 const toggleButton = document.getElementById("toggle-skills") as HTMLElement;
 const skills = document.getElementById("skills") as HTMLElement;
 toggleButton.addEventListener("click", ()=>{
-    if(skills.style.display == "none" || !skills.style.display){
-        skills.style.display = "block";
+    if(skills.style.display == "block" || !skills.style.display){
+        skills.style.display = "none";
 
     }else{
-        skills.style.display = "none"
+        skills.style.display = "block"
     }
    
 })
